ca639544100cf38b8861e7e22c6d3d009d005c05
William Alexander <williamalexander@Williams-MacBook-Air.local>
2024-09-11 15:13:50 +0700

    add tugas

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://http://william-alexander31-mentalhealthtracker.pbp.cs.ui.ac.id/", "https://http://william-alexander31-mentalhealthtracker.pbp.cs.ui.ac.id/"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://william-alexander31-mentalhealthtracker.pbp.cs.ui.ac.id/", "https://william-alexander31-mentalhealthtracker.pbp.cs.ui.ac.id/"]
